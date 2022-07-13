fd65495380b6a576a24319a5b3f79c3df024065a
Tim-Nicholson <t.r.nicho@gmail.com>
2022-07-13 12:06:53 -0400

    troubleshooting fullpage.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { FullPageWrapper } from './pages/FullPageWrapper';
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+import {Box} from "@mui/material"
+import LandingPage from './components/LandingPage';
+import { Fullpage } from './pages/FullPage';
 
 function App() {
   return (
     <div className="App">
-      <FullPageWrapper/>
+      <Fullpage />
     </div>
   );
 }
--- src/pages/FullPageWrapper.jsx
+++ src/pages/FullPageWrapper.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactFullpage from "@fullpage/react-fullpage";
-import LandingPage from "../components/LandingPage";
-import {Box} from "@mui/material"
 
 const anchors = ["landingpage", "skillspage", "projectpage", "resumepage"]
 
-const FullPageWrapper = () => {
+export const FullPageWrapper = () => {
     <ReactFullpage
+    licenseKey = {'YOUR_KEY_HERE'}
     anchors = {anchors}
     navigation
     navigationTooltips={anchors}
-    sectionsColor={["#282c34", "#ff5f45", "#0798ec", "#282c34"]}
+    sectionsColor={['#282c34', '#ff5f45', '#0798ec', '#282c34']}
     onLeave={(origin, destination, direction) => {
         console.log("onLeave event", { origin, destination, direction })
     }}
@@ -18,9 +15,12 @@ const FullPageWrapper = () => {
         console.log("render prop change", state, fullpageApi);
 
         return (
-            <ReactFullpage.Wrapper>
-                <h3>hi</h3>
-            </ReactFullpage.Wrapper>
+            <div>
+                <LandingPage/>
+                <LandingPage/>
+                <LandingPage/>
+                <LandingPage/>
+            </div>
         )
     }}
     />
