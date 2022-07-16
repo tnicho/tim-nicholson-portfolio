1c61caa2258ad25323244c1f29a67df84a574c7a
Tim-Nicholson <t.r.nicho@gmail.com>
2022-07-16 17:27:24 -0400

    project pictures added

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -8,7 +8,7 @@ export default function ProjectsPage(props){
             <Typography>Uses a slider for the projects you present</Typography>
             <Typography>Show Options along the bottom as cards and selected as larger above with info and links</Typography>
             <Typography>Have show include a Slider of Screenshots</Typography>
-            <Typography>Or USe Animations. Slide out the main slide and slide in the selected while using React-animations to show selected square</Typography>
+            <Typography>Or USe Animations. Slide out the main slide(component) and slide in the selected while using React-animations to show selected square</Typography>
         </Box>
     )
 }
