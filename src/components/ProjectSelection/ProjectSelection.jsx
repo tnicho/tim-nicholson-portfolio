bab2e1115ba22dcfae7410e4312459b5931dacfe
Tim-Nicholson <t.r.nicho@gmail.com>
2022-07-17 10:50:08 -0400

    added project card info

--- src/components/ProjectSelection/ProjectSelection.jsx
+++ src/components/ProjectSelection/ProjectSelection.jsx
@@ -1,4 +1,4 @@
-import {Box, Typography } from "@mui/material"
+import {Box, Experimental_CssVarsProvider, Typography } from "@mui/material"
 import React from "react"
 import ProjectSelectionCard from "../ProjectSelectionCard/ProjectSelectionCard"
 
@@ -11,10 +11,30 @@ export default function ProjectSelection(props) {
           display:'flex'
         }}
       >
-        <ProjectSelectionCard title= "LiftOff"/>
-        <ProjectSelectionCard title= "My Year Of Media"/>
-        <ProjectSelectionCard title ="Digipets Forever"/>
-        <ProjectSelectionCard title = "Ampless"/>
+        <ProjectSelectionCard 
+          title= "LiftOff" 
+          imagesrc ="./Portfolio Pics/Browser Game/Browser 1 (1).png" 
+          description = "A quicktime based Browser Game"
+          technologies = "JAvascript, HTML, CSS"
+          />
+        <ProjectSelectionCard 
+          title= "My Year Of Media" 
+          imagesrc="./Portfolio Pics/MYIM/myyearofmedia HomePage.png"
+          description = "A full CRUD, API-based, media tracker for Desktop or Mobile"
+          technologies = "Express, Node.js, MongoDB, HTML, CSS, JavaScript, Oauth, GoogleBooks API, TMDB API"
+          />
+        <ProjectSelectionCard 
+          title ="Digipets Forever" 
+          imagesrc="./Portfolio Pics/DigiPets Forever/Digipets Show.png"
+          description = 'A full CRUD, "pet" creation, app for mobile made as part of a team of UX and programmers'
+          technologies ="Python, Django, HTML, CSS, TailwindCSS, PostgreSQL"
+          />
+        <ProjectSelectionCard 
+        title = "Ampless" 
+        imagesrc = "./Portfolio Pics/Ampless/Ampless Main Page.png"
+        description = "A full CRUD, MERN web app for desktop to manipulate audio inputs"
+        technologies = "Express, Node,js, MongoDB, React, Material UI, Web Audio API"
+        />
       </Box>
     </Box>
 )
--- src/components/ProjectSelectionCard/ProjectSelectionCard.jsx
+++ src/components/ProjectSelectionCard/ProjectSelectionCard.jsx
@@ -1,10 +1,16 @@
 import React from "react"
-import {Box, Card, Typography} from "@mui/material"
+import {Box, Card, CardMedia, CardContent, Typography} from "@mui/material"
 
 export default function ProjectSelectionCard (props){
-    return(
-        <Card sx={{width:250}}>
-            <Typography>{props.title}</Typography>
-        </Card>
-    )
+  return(
+    <Card sx={{width:250}}>
+      <CardMedia
+        component="img"
+        image={props.imagesrc}
+      />
+      <CardContent>
+        <Typography>{props.title}</Typography>
+      </CardContent>
+    </Card>
+  )
 }
