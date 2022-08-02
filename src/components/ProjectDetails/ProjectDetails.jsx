b8445fcefdb5a5caa492b554b4ad60bcecf28d7a
Tim-Nicholson <t.r.nicho@gmail.com>
2022-08-02 12:59:39 -0400

    Project Details began

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Link} from '@mui/material'
 
 export default function ProjectDetails() {
   return (
-    <Box>
-        <Typography>ProjectDetails</Typography>
+    <Box
+    sx={{
+      mr: 20,
+      my: 10,
+      background: "blue",
+      height: '60vh',
+      width: '30vw'
+    }}
+    >
+      <Typography>ProjectDetails</Typography>
+      <Box
+        sx={{
+          background: "#ADD8E6"
+        }}>
+          Description: The goal of LIFT OFF is to help your "Weightlifter" complete a "Clean and Press" in 
+          which one lifts a bar to the waist, the chest, and finally over the head. This goal is 
+          accomplished through a series of reflex-based mini games using the arrow keys (or buttons on mobile).
+      </Box>
+      <Box
+        sx={{
+            background: "#00008B",
+            color: "white"
+          }}>
+            Technology Used: HTML, CSS, JavaScript, GitHub (hosting), and Canva (image creation)
+      </Box>
+      <Box
+        sx={{
+            background: "dark blue"
+          }}>
+            Links: <br/>
+            Deployed: <Link href="https://tnicho.github.io/BrowserGame-LiftOff/">tnicho.github.io/BrowserGame-LiftOff</Link><br/>
+            Github: <Link href="https://github.com/tnicho/BrowserGame-LiftOff">github.com/tnicho/BrowserGame-LiftOff</Link>
+      </Box>
     </Box>
   )
 }
