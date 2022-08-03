a012929c78bb2b411fd1dccc65367a61024a45d7
Tim-Nicholson <t.r.nicho@gmail.com>
2022-08-03 17:56:35 -0400

    Started including states for project Screen

--- src/components/ProjectDisplay/ProjectDisplay.jsx
+++ src/components/ProjectDisplay/ProjectDisplay.jsx
@@ -12,14 +12,12 @@ export default function ProjectDisplay() {
     >
       <ScreenshotDisplayBox/>
       <ProjectDetails 
-      title = 'LIFT OFF'
-      description = 'The goal of LIFT OFF is to help your "Weightlifter" complete a "Clean and Press" in 
-      which one lifts a bar to the waist, the chest, and finally over the head. This goal is 
-      accomplished through a series of reflex-based mini games using the arrow keys (or buttons on mobile).'
-      tech = 'HTML, CSS, JavaScript, GitHub, Canva'
+      title = "LIFT OFF"
+      description = "A mobile compatible browser game that The goal of LIFT OFF is to help your Weightlifter complete a Clean and Press in which one lifts a bar to the waist the chest and finally over the head This goal is accomplished through a series of reflex-based mini games using the arrow keys or buttons on mobile"
+      tech = "HTML, CSS, JavaScript, GitHub, Canva"
       linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
-      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"/>
-      
+      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"
+      />
     </Box>
   )
 }
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,18 +1,59 @@
-import React from "react"
+import React, { Component } from "react"
 import {Typography, Box} from "@mui/material"
 import ProjectSelection from "../../components/ProjectSelection/ProjectSelection"
 import ProjectDisplay from "../../components/ProjectDisplay/ProjectDisplay"
 
-export default function ProjectsPage(props){
-  return(
-    <Box>
-      <Typography>Projects  Page</Typography>
-      {/* <Typography>Uses a  slider for the projects you present</Typography>
-      <Typography>Show Options along the bottom as cards and selected as larger above with info and links</Typography>
-      <Typography>Have show include a Slider of Screenshots</Typography>
-      <Typography>Or USe Animations. Slide out the main slide(component) and slide in the selected while using React-animations to show selected square</Typography> */}
-      <ProjectSelection/>
-      <ProjectDisplay/>
-    </Box>
-)
+export default class ProjectsPage extends Component{
+  state = {
+    project: {title: "AMPLESS",
+    description: "AMPLESS description",
+    tech: "AMPLESS tech",
+    linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
+    linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"}
+  };
+
+  handleChange = (evt) => {
+    const liftoff ={
+      title: "LIFT OFF",
+      description: "liftoff description",
+      tech: "liftoff tech",
+      linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
+      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"
+    }
+    const myYearInMedia ={
+      title: "MY YEAR IN MEDIA",
+      description: "myim description",
+      tech: "myim tech",
+      linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
+      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"
+    }
+    const digipetsForever ={
+      title: "DIGIPETS FOREVER",
+      description: "DIGIPETS FOREVER description",
+      tech: "DIGIPETS FOREVER tech",
+      linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
+      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"
+    }
+    const ampless ={
+      title: "AMPLESS",
+      description: "AMPLESS description",
+      tech: "AMPLESS tech",
+      linkDeployed ="https://tnicho.github.io/BrowserGame-LiftOff/"
+      linkGithub = "https://github.com/tnicho/BrowserGame-LiftOff"
+    }
+  };
+
+  render(){
+    return(
+      <Box>
+        <Typography>Projects  Page</Typography>
+        {/* <Typography>Uses a  slider for the projects you present</Typography>
+        <Typography>Show Options along the bottom as cards and selected as larger above with info and links</Typography>
+        <Typography>Have show include a Slider of Screenshots</Typography>
+        <Typography>Or USe Animations. Slide out the main slide(component) and slide in the selected while using React-animations to show selected square</Typography> */}
+        <ProjectSelection/>
+        <ProjectDisplay/>
+      </Box>
+    )
+  }
 }
