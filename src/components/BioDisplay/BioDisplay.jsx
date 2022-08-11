8a8ca4ae441b3b9a7703db0f4a4a78cf9c50889f
Tim-Nicholson <t.r.nicho@gmail.com>
2022-08-11 18:29:36 -0400

    editted slides

--- src/components/BioDisplay/BioDisplay.jsx
+++ src/components/BioDisplay/BioDisplay.jsx
@@ -3,10 +3,16 @@ import {Paper, Typography} from "@mui/material"
 
 export default function BioDisplay() {
   return (
-    <Paper
-    sx={{margin:'10px'}}>
-      <Typography>Biography Display</Typography>
-      <Typography>A Paragraphy about me, and my goals, and journey</Typography>
+    <Paper 
+      sx={{
+        margin:'10px', 
+        width: '40vw', 
+        height: '60vh', 
+        display: 'flex', 
+        flexDirection:'column', 
+      }}>
+      <Typography variant='h3'>Biography Display</Typography>
+      <Typography variant="h5">I am a full-stack developer with a history in engineering and a fascination with the intersection of technology and the arts. Leaving my career as an engineer led me to following a passion for coding into a career in full-stack development.</Typography>
     </Paper>
   )
 }
--- src/components/SkillsDisplay/SkillsDisplay.jsx
+++ src/components/SkillsDisplay/SkillsDisplay.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 
 export default function SkillsDisplay() {
   return (
-    <Paper sx={{margin:'10px', width: '40vw', height: '60vh'}}>
-      <Typography>SkillsDisplay</Typography>
-      <Box>
+    <Paper sx={{margin:'10px', width: '40vw', height: '60vh', display: 'flex', flexDirection:'column', justifyContent:'space-evenly'}}>
+      <Typography variant='h3'>TECHNOLOGIES</Typography>
         <Box>
-          <Typography>Front End:</Typography>
+          <Typography variant="h5">Front End:</Typography>
           <Box
-          sx={{display: 'flex', width:'20vw', height: '25vh'}}>
+            sx={{display: 'flex', justifyContent: 'space-evenly', width:'40vw'}}>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" />
@@ -17,20 +16,18 @@ export default function SkillsDisplay() {
           </Box>
         </Box>
         <Box>
-          <Typography>Back End:</Typography>
+          <Typography variant="h5">Back End:</Typography>
           <Box
-          sx={{display: 'flex', width:'20vw', height: '25vh'}}>
+          sx={{display: 'flex', justifyContent: 'space-evenly', width:'40vw'}}>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"/>
           </Box>
         </Box>
-      </Box>
-      <Box sx={{display:'flex'}}>
         <Box>
-          <Typography>Frameworks:</Typography>
+          <Typography variant="h5">Frameworks:</Typography>
           <Box
-          sx={{display: 'flex', width:'20vw', height: '25vh'}}>
+          sx={{display: 'flex', justifyContent: 'space-evenly', width:'40vw'}}>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"/>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"/>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
@@ -38,16 +35,15 @@ export default function SkillsDisplay() {
           </Box>
         </Box>
         <Box>
-          <Typography>Technologies:</Typography>
+          <Typography variant="h5">Technologies:</Typography>
           <Box
-          sx={{display: 'flex', width:'20vw', height: '25vh'}}>
+          sx={{display: 'flex', justifyContent: 'space-evenly', width:'40vw'}}>
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
             <img height='75px' width='75px' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gimp/gimp-original.svg" />
           </Box>
         </Box>
-      </Box>
     </Paper>
       
   )
