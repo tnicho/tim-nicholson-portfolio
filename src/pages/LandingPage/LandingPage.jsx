df047271adaff94cafa56fb7c1d9b15d9d87682e
Tim-Nicholson <t.r.nicho@gmail.com>
2022-08-22 14:54:54 -0400

    landing page Logo Color

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,34 +1,46 @@
 import React, {Component} from "react"
 import {Box, Typography} from "@mui/material"
-import { red } from "@mui/material/colors"
+import randomColor from "randomcolor"
+import {ReactComponent as TimLogo} from '../../1.svg';
 
 export default class LandingPage extends Component {
   state={
-    logoNumber: '1'
+    logoNumber: '1',
+    logoBackgroundColour: 'white',
+    logoFillColour: 'black',
+    logoStrokeColour: "black"
   }
 
   handleLogoChange = (evt) =>{
-    if (this.state.logoNumber ==='1'){
-      this.setState({
-       logoNumber:'2'
-    })
-    }else if (this.state.logoNumber ==='2'){
-      this.setState({
-      logoNumber:'3'
-    })
-    }else if (this.state.logoNumber ==='3'){
-      this.setState({
-      logoNumber:'4'
-    })
-    }else if (this.state.logoNumber ==='4'){
-      this.setState({
-      logoNumber:'5'
-    })
-    }else if (this.state.logoNumber ==='5'){
-      this.setState({
-      logoNumber:'1'
-    })
-    }
+    const newBackgroundColor = randomColor()
+    const newFillColor = randomColor()
+    const newStrokeColor = randomColor()
+    // if (this.state.logoNumber ==='1'){
+    //   this.setState({
+    //    logoNumber:'2'
+    // })
+    // }else if (this.state.logoNumber ==='2'){
+    //   this.setState({
+    //   logoNumber:'3'
+    // })
+    // }else if (this.state.logoNumber ==='3'){
+    //   this.setState({
+    //   logoNumber:'4'
+    // })
+    // }else if (this.state.logoNumber ==='4'){
+    //   this.setState({
+    //   logoNumber:'5'
+    // })
+    // }else if (this.state.logoNumber ==='5'){
+    //   this.setState({
+    //   logoNumber:'1'
+    // })
+    // }
+    this.setState({
+         logoBackgroundColour: newBackgroundColor,
+         logoFillColour: newFillColor,
+         logoStrokeColour: newStrokeColor
+      })
 
   }
   render (){ 
@@ -42,11 +54,10 @@ export default class LandingPage extends Component {
           justifyContent: 'center',
         }}
       >
-        <Typography>Landing Page</Typography>
-        <Typography>Put a large Translucent Arrow at bottom and top of each page. Maybe</Typography>
         <Box
         sx={{
-          backgroundColor: 'red',
+          my: '10vh',
+          backgroundColor: this.state.logoBackgroundColour,
           '&:hover': {
             cursor: 'pointer'
           },
@@ -54,9 +65,17 @@ export default class LandingPage extends Component {
           onClick= {this.handleLogoChange}
         >
           {/* <img src = {"./Logo Colours/" + this.state.logoNumber + ".png"}/> */}
-          <img src = {"./1.svg"}/>
+          <TimLogo fill={this.state.logoFillColour} stroke={this.state.logoStrokeColour}/>
         </Box>
+        <Box
+          sx={{
+            '&:hover': {
+              cursor: 'pointer'
+            },
+          }}
+        >
         <Typography>Welcome</Typography>
+        </Box>
       </Box>
     )
   }
