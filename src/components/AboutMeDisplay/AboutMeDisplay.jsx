2ef530158e359e826a66dad37fd8ca928cc2b546
Tim-Nicholson <t.r.nicho@gmail.com>
2022-09-02 10:01:36 -0400

    fixing AboutMe Menu

--- src/components/AboutMeDisplay/AboutMeDisplay.jsx
+++ src/components/AboutMeDisplay/AboutMeDisplay.jsx
@@ -14,17 +14,17 @@ export default function AboutMeDisplay(props) {
       ml: '10vw'
     }}
     >
-        <Slide direction="right" appear={true} mountOnEnter unmountOnExit in={props.bioShow} timeout={{ enter: 1000, exit: 1000}}>
+        <Slide direction="left" appear={true} mountOnEnter unmountOnExit in={props.bioShow} timeout={{ enter: 1000, exit: 1000}}>
           <Box>
             <BioDisplay elevation={props.elevation}/>
           </Box>
         </Slide>
-        <Slide direction="right" appear={true} mountOnEnter unmountOnExit in={props.technologyShow} timeout={{ enter: 1000, exit: 1000}}>
+        <Slide direction="left" appear={true} mountOnEnter unmountOnExit in={props.technologyShow} timeout={{ enter: 1000, exit: 1000}}>
         <Box >
           <SkillsDisplay elevation={props.elevation}/>
         </Box>
         </Slide>
-        <Slide direction="right" appear={true} mountOnEnter unmountOnExit in={props.educationShow} timeout={{ enter: 1000, exit: 1000}}>
+        <Slide direction="left" appear={true} mountOnEnter unmountOnExit in={props.educationShow} timeout={{ enter: 1000, exit: 1000}}>
         <Box >
           <EducationDisplay elevation={props.elevation}/>
         </Box>
