ceb1c2711eeb9d1f191fa3fd80e9d1e6be686e59
Tim-Nicholson <t.r.nicho@gmail.com>
2022-09-01 22:02:55 -0400

    Troubleshooting ghpages deploy

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={'https://tnicho.github.io/tim-nicholson-portfolio/'}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
