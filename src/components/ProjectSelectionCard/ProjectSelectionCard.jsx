746ed6847e45e132ba7a557b708831c2bfc59071
Tim-Nicholson <t.r.nicho@gmail.com>
2022-09-02 08:04:20 -0400

    fixing project selection card size

--- src/components/ProjectSelectionCard/ProjectSelectionCard.jsx
+++ src/components/ProjectSelectionCard/ProjectSelectionCard.jsx
@@ -3,7 +3,7 @@ import {Box, Button, Card, CardMedia, CardContent, CardActions,  CardActionArea,
 
 export default function ProjectSelectionCard (props){
   return(
-    <Card elevation={10} sx={{width:16}} >
+    <Card elevation={10} sx={{width:160}} >
       <CardActionArea
         onClick = {props.handleProjectSelection}
       >
