c9eadc5d2b01c088f58185df8968ca3fb33c8b49
Tim-Nicholson <t.r.nicho@gmail.com>
2022-09-06 14:46:23 -0400

    fixing mobile about me paper widths and button colors

--- src/components/AboutMeButtons/AboutMeButtons.jsx
+++ src/components/AboutMeButtons/AboutMeButtons.jsx
@@ -6,14 +6,14 @@ export default function AboutMeButtons(props) {
     <Box
         sx={{display:'flex'}}
     >
-      <Button sx={{mt: '10px', backgroundColor: "secondary.light", color:'primary.main', fontWeight: 'bold'}}variant ="contained" size="large" id="biography" onClick = {props.handleAboutChange} >BIOGRAPHY</Button>
+      <Button sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large" id="biography" onClick = {props.handleAboutChange} >BIOGRAPHY</Button>
         <Button
-          sx={{mt: '10px', backgroundColor: "secondary.light", color:'primary.main', fontWeight: 'bold'}}variant ="contained" size="large"
+          sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large"
           onClick = {props.handleAboutChange}
           id="technology"
         >Technology</Button>
         <Button
-          sx={{mt: '10px', backgroundColor: "secondary.light", color:'primary.main', fontWeight: 'bold'}}variant ="contained" size="large"
+          sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large"
           onClick = {props.handleAboutChange}
           id="education"
         >Education</Button>
