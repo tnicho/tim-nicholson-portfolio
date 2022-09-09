9f2540dd3599713214096f954663700baece2927
Tim-Nicholson <t.r.nicho@gmail.com>
2022-09-09 11:02:32 -0400

    completed portfolio mobile

--- src/components/AboutMeButtons/AboutMeButtons.jsx
+++ src/components/AboutMeButtons/AboutMeButtons.jsx
@@ -3,21 +3,6 @@ import { Box, Button, Grid } from '@mui/material'
 
 export default function AboutMeButtons(props) {
   return (
-    // <Box
-    //     sx={{display:'flex'}}
-    // >
-    //   <Button sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large" id="biography" onClick = {props.handleAboutChange} >BIOGRAPHY</Button>
-    //     <Button
-    //       sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large"
-    //       onClick = {props.handleAboutChange}
-    //       id="technology"
-    //     >Technology</Button>
-    //     <Button
-    //       sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large"
-    //       onClick = {props.handleAboutChange}
-    //       id="education"
-    //     >Education</Button>
-    // </Box>
     <Grid container spacing={1} sx={{my:1}}>
     <Grid item xs={6} sx={{display:'flex', justifyContent:'center'}}>
     <Button sx={{mt: '10px', backgroundColor: 'primary.main', color:"secondary.light", fontWeight: 'bold'}}variant ="contained" size="large" id="biography" onClick = {props.handleAboutChange} >BIOGRAPHY</Button>
